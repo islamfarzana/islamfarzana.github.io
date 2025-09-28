3c7282a649b2ccfd7824eda2e479d44ad6f43be7
Farzana Islam <islamfarzana124@gmail.com>
2025-09-28 22:58:47 +0600

    updated index

---
+++ Tea-House/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./**/*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
